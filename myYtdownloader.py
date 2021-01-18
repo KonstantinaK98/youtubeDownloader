7b9a5eca7a8258e9dd038e4b6a5bcce7222fb478
KonstantinaK98 <konakara98@gmail.com>
2021-01-19 01:37:22 +0200

    Initialization of the window

---
+++ myYtdownloader.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('500x350')
+root.title('My Youtube Downloader')
+
+myLabel = Label(root, text="Youtube Downloader")
+myLabel.pack()
+
+root.mainloop()
