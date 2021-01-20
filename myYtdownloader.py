282c84ab18f7340007e5bb9e8b351a36bdaaf7b5
KonstantinaK98 <konakara98@gmail.com>
2021-01-20 22:44:54 +0200

    Input field and download button added

--- myYtdownloader.py
+++ myYtdownloader.py
@@ -4,7 +4,13 @@ root = Tk()
 root.geometry('500x350')
 root.title('My Youtube Downloader')
 
-myLabel = Label(root, text="Youtube Downloader")
-myLabel.pack()
+titleLabel = Label(root, text="Youtube Downloader", font=("Arial" , 14), pady=10)
+titleLabel.pack()
+
+urlEntry = Entry(root, width=50)
+urlEntry.pack()
+
+downloadBtn = Button(root, text="Download", width=20, height=2)
+downloadBtn.pack()
 
 root.mainloop()
